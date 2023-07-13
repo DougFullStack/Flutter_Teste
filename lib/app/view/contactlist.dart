4b843694266b1825d225ccf42d24609f089fe02c
douglas <dublandoacena@gmail.com>
2023-07-13 10:36:01 -0300

    versao1

---
+++ lib/app/view/contactlist.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContactList extends StatelessWidget {
+  const ContactList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("FLUTTER"),
+      ),
+      body: ListView(),
+    );
+  }
+}
