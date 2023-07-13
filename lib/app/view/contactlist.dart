9d954ba2c28a63ca5847d892d62279e6f386128b
douglas <dublandoacena@gmail.com>
2023-07-13 20:34:10 -0300

    versao3

--- lib/app/view/contactlist.dart
+++ lib/app/view/contactlist.dart
@@ -2,7 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutterapp/app/my_app.dart';
 
 class ContactList extends StatelessWidget {
-  const ContactList({super.key});
+  final lista = [
+    {
+      "Nome": "LUCAS DEPRESSIVO",
+      "tel": "14998343434",
+      "Avatar":
+          "https://media.istockphoto.com/id/840110250/pt/foto/asian-man-in-despair.jpg?s=1024x1024&w=is&k=20&c=caA-oS6RZYu2zKVZ8KoTEQ_6OmYp1oPWcqXFVmZNKIs="
+    },
+    {
+      "Nome": "PEDRO TRISTÃO",
+      "tel": "14998343434",
+      "Avatar":
+          "https://media.istockphoto.com/id/665950252/pt/foto/sad-cheerless-man-covering-his-face.jpg?s=1024x1024&w=is&k=20&c=dSiXfNpiUvL1pvlg-9ORhSbYqKqYrOrXZPw7KCneDnY="
+    }
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +32,32 @@ class ContactList extends StatelessWidget {
           )
         ],
       ),
-      body: ListView(),
+      body: ListView.builder(
+          itemCount: lista.length,
+          itemBuilder: (context, i) {
+            var contato = lista[i];
+            var avatar = CircleAvatar(
+              backgroundImage: NetworkImage(contato["Avatar"] ?? ''),
+            );
+            return ListTile(
+              leading: avatar,
+              title: Text(contato["Nome"]?.toString() ?? ""),
+              subtitle: Text(contato['tel']?.toString() ?? ''),
+              trailing: Container(
+                width: 100,
+                child: const Row(
+                  children: [
+                    IconButton(onPressed: null, icon: Icon(Icons.edit)),
+                    IconButton(onPressed: null, icon: Icon(Icons.delete))
+                  ],
+                ),
+              ),
+            );
+            /* ?. >> este é um operador de acesso seguro, serve para verificar se
+            o valor será nulo ou não.
+            ?? >> já este operador me diz que irá retornar uma string vazia
+            por padrão caso ele seja nulo */
+          }),
     );
   }
 }
